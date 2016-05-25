6f618eae21e3f79681249c26c433217cc3e26649
Chris Compton <chris.compton@va.gov>
2016-05-25 13:02:45 -0500

    Updated for setup

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # Docker-CentOS
-Basics for spinning up CentOS 7 with systemd
+Basics for spinning up a container with CentOS 7, systemd and Nginx
+
+## Initial Commands from a clean CentOS 7 VM
+
+```
+yum install -y git
+cd ~/
+git clone https://github.com/ChrisCompton/Docker-CentOS.git
+cd Docker-CentOS
+cd scripts
+chmod 755 setup.sh
+./setup.sh
+```
+
+
+## For locking down SSH a bit
+
+Make sure to use your own public key! ssh.pub is mine!
+
+```
+umask 077 && mkdir -p ~/.ssh
+cat ssh.pub >> ~/.ssh/authorized_keys
+yum install -y perl
+perl -p -i -e 's/PasswordAuthentication yes/PasswordAuthentication no/g' /etc/ssh/sshd_config
+perl -p -i -e 's/#PasswordAuthentication no/PasswordAuthentication no/g' /etc/ssh/sshd_config
+service sshd restart
+```
--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,15 +1,3 @@
-#yum install -y git
-#cd ~/
-#git clone https://github.com/ChrisCompton/Docker-CentOS.git
-#cd Docker-CentOS
-
-#umask 077 && mkdir -p ~/.ssh
-#cat ssh.pub >> ~/.ssh/authorized_keys
-#yum install -y perl
-#perl -p -i -e 's/PasswordAuthentication yes/PasswordAuthentication no/g' /etc/ssh/sshd_config
-#perl -p -i -e 's/#PasswordAuthentication no/PasswordAuthentication no/g' /etc/ssh/sshd_config
-#service sshd restart
-
 yum update -y
 sudo tee /etc/yum.repos.d/docker.repo <<-'EOF'
 [dockerrepo]
@@ -25,7 +13,5 @@ yum install -y docker-engine
 service docker start
 
 docker build --rm -t c7-base ../centos
-#docker run -ti --privileged=true -v /sys/fs/cgroup:/sys/fs/cgroup:ro -p 80:80 --name test c7-base
-
 docker build --rm -t c7-nginx ../nginx
 docker run -ti --privileged=true -v /sys/fs/cgroup:/sys/fs/cgroup:ro -p 80:80 --name test c7-nginx
